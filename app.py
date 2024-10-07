31301a3d573b4dcd635163f73b9fdf94aa64909f
TrungKhanhNguyen <khanhnt92@gmail.com>
2024-10-07 21:32:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_meme_coin_token(block_height):
     upshot_url = f"https://api.upshot.xyz/v2/allora/tokens-oracle/token/{block_height}"
     headers = {
         "accept": "application/json",
-        "x-api-key": "UP-9d2ac0fea0a244fda624db9c"  # replace with your API key
+        "x-api-key": "UP-4b008a1aaf194a27b65dcf15"  # replace with your API key
     }
 
     response = requests.get(upshot_url, headers=headers)
