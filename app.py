babfc8fcd18a6c576321cb00f7cd8156beb4125e
TrungKhanhNguyen <khanhnt92@gmail.com>
2024-10-08 06:18:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,55 +3,52 @@ import random
 
 import requests
 from flask import Flask, Response
-from model import train_model, download_data_for_meme_coins, format_data_for_meme_coins
-from model import forecast_price
-
 app = Flask(__name__)
 
-
-def get_token_inference_for_meme(token, network):
-    download_data_for_meme_coins(token, network)
-    format_data_for_meme_coins(token)
-    train_model(token)
-    return forecast_price.get(token, 0)
 
 
 def get_meme_coin_token(block_height):
     upshot_url = f"https://api.upshot.xyz/v2/allora/tokens-oracle/token/{block_height}"
     headers = {
         "accept": "application/json",
-        "x-api-key": "UPAPIKEY"  # replace with your API key
+        "x-api-key": "UP-4b008a1aaf194a27b65dcf15"  # replace with your API key
     }
 
     response = requests.get(upshot_url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
-        name_token = str(data["data"]["address"])  # return "boshi"
-        network = str(data["data"]["platform"])
-        return name_token, network
+        name_token = str(data["data"]["token_id"])  # return "boshi"
+        return name_token
     else:
         raise ValueError("Unsupported token")
 
 
 @app.route("/inference/<string:token_or_block_height>")
 def get_inference(token_or_block_height):
-    if token_or_block_height.isnumeric():
-        name_coin, network = get_meme_coin_token(token_or_block_height)
+        name_coin = get_meme_coin_token(token_or_block_height)
         try:
-            inference = get_token_inference_for_meme(name_coin, network)
+            base_url = "https://api.coingecko.com/api/v3/simple/price?ids="
+            url = f"{base_url}{current_token}&vs_currencies=usd"
+            headers = {
+            "accept": "application/json",
+            "x-cg-demo-api-key": "API"  # replace with your API key
+            }
+
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                data = response.json()
+                tempValue = str(data[token_id]["usd"])
+                
+                current_price = float(tempValue)
+                adjustment_factor = random.uniform(0.99, 1.01)
+                adjusted_price = current_price * adjustment_factor
+                return str(format(adjusted_price, ".7f"))
             # return str(inference)
             return Response(str(inference), status=200)
         except Exception as e:
             return Response(json.dumps({"error": str(e)}), status=500, mimetype='application/json')
-    else:
-        name_coin = token_or_block_height
-        try:
-            inference = get_last_price(name_coin, get_simple_price(name_coin))
-            # return inference
-            return Response(str(inference), status=200)
-        except Exception as e:
-            return Response(json.dumps({"error": str(e)}), status=500, mimetype='application/json')
+
 
 
 @app.route("/healthcheck")
